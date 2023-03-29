c6d0d1dd08d097a6235ed456dc6ad311b6fc4324
guillsou <35000004+GuillaumeGSO@users.noreply.github.com>
2023-03-29 22:15:25 +0800

    Working copy, rotate and flip

--- pick_blocks.py
+++ pick_blocks.py
@@ -9,7 +9,7 @@ http://simpson.edu/computer-science/
 Explanation video: http://youtu.be/iwLj7iJCFQM
 """
 import pygame
-import random
+import copy
 from math import cos, sin, sqrt, pi
 from random import randint
 
@@ -18,7 +18,7 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 
-SIZE = 100
+SIZE = 50
 
 
 class Tile(pygame.sprite.Sprite):
@@ -28,19 +28,39 @@ class Tile(pygame.sprite.Sprite):
         # Call the parent class (Sprite) constructor
         super().__init__()
 
-        surf4 = pygame.Surface((size * 3.1, size * 2.3), pygame.SRCALPHA)
+        self.color = color
+        self.size = size
+        self.rot = 0
+
+        self.init_surf = pygame.Surface(
+            (size * 3.1, size * 2.3), pygame.SRCALPHA)
         # surf4.fill("pink")
-        # pygame.draw.polygon(surf4, color, self.draw_hat(
-        #     surf4.get_width() / 2, surf4.get_height() / 4.5, size))
-        pygame.draw.aalines(surf4, color, True, self.draw_hat(
-            surf4.get_width() / 2, surf4.get_height() / 4.5, size))
-        self.image = surf4
+        # pygame.draw.polygon(self.init_surf, self.color, self.draw_hat(
+        #     self.init_surf.get_width() / 2, self.init_surf.get_height() / 4.5, size))
+        pygame.draw.aalines(self.init_surf, self.color, True, self.draw_hat(
+            self.init_surf.get_width() / 2, self.init_surf.get_height() / 4.5, size))
+        self.image = self.init_surf
 
         # Fetch the rectangle object that has the dimensions of the image
         # image.
         # Update the position of this object by setting the values
         # of rect.x and rect.y
         self.rect = self.image.get_rect()
+
+    def rotate_left(self):
+        self.rot += 30
+        self.image = self.init_surf.copy()
+        self.image = pygame.transform.rotate(self.image, self.rot)
+
+    def rotate_right(self):
+        self.rot -= 30
+        self.image = self.init_surf.copy()
+        self.image = pygame.transform.rotate(self.image, self.rot)
+
+    def flip(self):
+        image_copy = self.image.copy()
+        self.image_copy = pygame.transform.rotate(image_copy, self.rot)
+        self.image = pygame.transform.flip(image_copy, True, False)
 
     def draw_hat(self, x, y, size):
 
@@ -75,9 +95,9 @@ class Tile(pygame.sprite.Sprite):
                    point11[1] - half * sin(pi / 3))
         point13 = (point12[0] + apotheme * cos(pi / 6),
                    point12[1] - apotheme * sin(pi / 6))
-        print("size", size)
-        print(point4[0]-point12[0])
-        print(point8[1]-point13[1])
+        # print("size", size)
+        # print(point4[0]-point12[0])
+        # print(point8[1]-point13[1])
         return [point1, point2, point3, point4,
                 point5, point6, point7, point8, point9, point10, point11, point12, point13]
 
@@ -86,28 +106,14 @@ class Player(Tile):
     """ This class represents the player. It derives from block and thus gets
     the same ___init___ method we defined above. """
 
-    # List of all the blocks we are carrying
-    carry_block_list = []
-
     def update(self):
-        """ Method called when updating a sprite. """
+        # """ Method called when updating a sprite. """
 
-        # Get the current mouse position. This returns the position
-        # as a list of two numbers.
+        # # Get the current mouse position. This returns the position
+        # # as a list of two numbers.
         pos = pygame.mouse.get_pos()
 
-        # Now see how the mouse position is different from the current
-        # player position. (How far did we move?)
-        diff_x = self.rect.x - pos[0]
-        diff_y = self.rect.y - pos[1]
-
-        # Loop through each block that we are carrying and adjust
-        # it by the amount we moved.
-        for block in self.carry_block_list:
-            block.rect.x -= diff_x
-            block.rect.y -= diff_y
-
-        # Now wet the player object to the mouse location
+        # # Now wet the player object to the mouse location
         self.rect.x = pos[0]
         self.rect.y = pos[1]
 
@@ -116,8 +122,8 @@ class Player(Tile):
 pygame.init()
 
 # Set the height and width of the screen
-screen_width = 700
-screen_height = 400
+screen_width = 800
+screen_height = 600
 screen = pygame.display.set_mode([screen_width, screen_height])
 
 # This is a list of 'sprites.' Each block in the program is
@@ -128,17 +134,6 @@ block_list = pygame.sprite.Group()
 # All blocks and the player block as well.
 all_sprites_list = pygame.sprite.Group()
 
-for i in range(randint(10, 20)):
-    # This represents a block
-    block = Tile(BLACK, SIZE)
-
-    # Set a random location for the block
-    block.rect.x = random.randrange(screen_width)
-    block.rect.y = random.randrange(screen_height)
-
-    # Add the block to the list of objects
-    block_list.add(block)
-    all_sprites_list.add(block)
 
 # Create a RED player block
 player = Player(RED, SIZE)
@@ -160,24 +155,25 @@ while not done:
             done = True
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            # When the mouse button is pressed, see if we are in contact with
-            # other sprites:
-            blocks_hit_list = pygame.sprite.spritecollide(
-                player, block_list, False)
+            tile = Tile(WHITE, player.size)
+            tile.image = player.image.copy()
+            tile.rect = player.rect.copy()
+            all_sprites_list.add(tile)
 
-            # Set the list of blocks we are in contact with as the list of
-            # blocks being carried.
-            player.carry_block_list = blocks_hit_list
-
-        elif event.type == pygame.MOUSEBUTTONUP:
-            # When we let up on the mouse, set the list of blocks we are
-            # carrying as empty.
-            player.carry_block_list = []
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                player.rotate_right()
+            if event.key == pygame.K_LEFT:
+                player.rotate_left()
+            if event.key == pygame.K_SPACE:
+                player.flip()
+            if event.key == pygame.K_ESCAPE:
+                done = True
 
     all_sprites_list.update()
 
     # Clear the screen
-    screen.fill(WHITE)
+    screen.fill(BLACK)
 
     # Draw all the spites
     all_sprites_list.draw(screen)
